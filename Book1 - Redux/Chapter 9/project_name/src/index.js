37b98720eebef57534704731928cacb6bee54c9e
Darrel2018 <darrelp2000@gmail.com>
2026-03-19 11:17:06 +0200

    fixed issues

--- Book1 - Redux/Chapter 9/project_name/src/index.js
+++ Book1 - Redux/Chapter 9/project_name/src/index.js
@@ -14,14 +14,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA54Du59KHUkRMqXeUHB7sX2hxN0rYWCdM",
-  authDomain: "user-app-69e39.firebaseapp.com",
-  projectId: "user-app-69e39",
-  storageBucket: "user-app-69e39.firebasestorage.app",
-  messagingSenderId: "324032336944",
-  appId: "1:324032336944:web:be9a1c75d6a86a5907c9f5",
-  measurementId: "G-BJNFT59LDM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
